db02d4429747e9dac20bd651505bed529189d84d
DietervanDortmont <dieterdort@gmail.com>
2026-05-02 08:51:06 +0200

    V63

--- src/Underlit/UI/SettingsWindow.xaml.cs
+++ src/Underlit/UI/SettingsWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Threading;
 using Underlit.Display;
 using Underlit.Settings;
 using Underlit.Sys;
